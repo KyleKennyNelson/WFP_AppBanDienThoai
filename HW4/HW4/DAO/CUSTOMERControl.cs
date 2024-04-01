b781b173245f342d4cae85bb987d4097ac78734b
KyleKennyNelson <147902202+KyleKennyNelson@users.noreply.github.com>
2024-04-01 12:44:38 +0700

    customer

---
+++ HW4/HW4/DAO/CUSTOMERControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HW4.DAO
+{
+    internal class CUSTOMERControl(SqlConnection connect)
+    {
+
+    }
+}
---
+++ HW4/HW4/DTO/CUSTOMER.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4.DTO
+{
+    internal class CUSTOMER : INotifyPropertyChanged, ICloneable
+    {
+        public string CusID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNum { get; set; }
+        public string CusAddress { get; set; }
+        public DateTime DOB { get; set; }
+        public string CusImage { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
